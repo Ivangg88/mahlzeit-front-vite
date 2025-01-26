ab8901aafcdbbe53c456e828c89f90120f8e0e42
Ivan Garcia <ivanggv2@gmail.com>
2025-01-26 19:56:19 +0100

    installed testing tools

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { configDefaults } from "vitest/config";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    ...configDefaults,
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./setupTests.ts", // Si usas un archivo de configuración
+  },
+});
